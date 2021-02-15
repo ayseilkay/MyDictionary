430f9e2acbb6566027dd80745caa4645053c1bbc
ayseilkay <aayseilkay@gmail.com>
2021-02-15 22:31:11 +0300

    Proje dosyası ekle.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    public class MyDictionary<TKey,TValue>
+    {
+        List<TKey> _keys;
+        List<TValue> _values;
+        public MyDictionary()
+        {
+            _keys = new List<TKey>();
+            _values = new List<TValue>();
+        }
+        public void Add(TKey key,TValue value)
+        {
+            _keys.Add(key);
+            _values.Add(value);
+           
+        }
+        public void Key(TKey key)
+        {
+            try
+            {
+                Console.WriteLine(_values[_keys.IndexOf(key)]);
+
+            }
+            catch
+            {
+                Console.WriteLine("key mevcut değil");
+
+            }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> myDictionary = new MyDictionary<string, string>();
+            myDictionary.Add("Hg", "Civa");
+            myDictionary.Add("Au", "Altın");
+            myDictionary.Add("Al", "Alüminyum");
+
+            myDictionary.Key("Hg");
+            myDictionary.Key("Au");
+            myDictionary.Key("Al");
+            myDictionary.Key("Ni");// bu key mevcut değil
+            Console.ReadLine();
+        }
+    }
+}
